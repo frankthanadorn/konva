a984461cd32fcf734e466662fc28f15551878891
frankthanadorn <72251422+frankthanadorn@users.noreply.github.com>
2021-02-23 14:36:22 +0700

    try to draw circle on image

--- konvatask/src/App.js
+++ konvatask/src/App.js
@@ -1,25 +1,12 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { Stage, Layer, Image } from 'react-konva';
-import useImage from 'use-image';
+// import React, { Component } from 'react';
 
-// the first very simple and recommended way:
-const LionImage = () => {
-  const [image] = useImage('http://128.199.244.46:4000/getimage?image=inferNone94323.jpeg');
-  return <Image image={image} />;
-};
-
-
-class App extends Component {
-  render() {
-    return (
-      <Stage width={360} height={438}>
-        <Layer>
-          <LionImage />
-        </Layer>
-      </Stage>
-    );
-  }
-}
-
-export default App;
+// class App extends Component {
+//   render() {
+//     return (
+//       <div classname="App">
+        
+//       </div>
+//     );
+//   }
+// }
+// export default App;
